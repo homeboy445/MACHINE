2b278e8e570881160e4dabcbff2fbc52c0fc95e3
Akshit <61937872+homeboy445@users.noreply.github.com>
2021-12-08 17:38:15 +0530

    Added a loader and updated the site title and the icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import Loader from "react-loader-spinner";
 import Home from "./components/Home/Home";
 import QueryEditor from "./components/QueryEditor/QueryEditor";
 import ShareResultPage from "./components/ShareResultPage/ShareResultPage";
@@ -12,14 +13,45 @@ const RedirectToHome = () => {
 };
 
 const App = () => {
+  const [loading, updateStatus] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      updateStatus(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div style={{ minHeight: "100vh", scrollBehavior: "smooth" }}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/editor" element={<QueryEditor />} />
-        <Route path="/sql-share/:id" element={<ShareResultPage />} />
-        <Route path="*" element={<RedirectToHome />} />
-      </Routes>
+    <div>
+      {loading ? (
+        <Loader
+          type="TailSpin"
+          color="#427ef7"
+          height={250}
+          width={250}
+          style={{
+            position: "absolute",
+            left: "40%",
+            top: "40%",
+          }}
+          timeout={4000}
+        />
+      ) : null}
+      <div
+        style={{
+          minHeight: "100vh",
+          scrollBehavior: "smooth",
+          opacity: loading ? 0.01 : 1,
+          pointerEvents: loading ? "none" : "all",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor" element={<QueryEditor />} />
+          <Route path="/sql-share/:id" element={<ShareResultPage />} />
+          <Route path="*" element={<RedirectToHome />} />
+        </Routes>
+      </div>
     </div>
   );
 };
